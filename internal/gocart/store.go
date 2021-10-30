9334ce6217a634591061738273dc4b8999b35d7e
Ben Aaron <ben@theaarons.com>
2021-10-30 10:29:37 -0700

    split storage into subpackage

---
+++ internal/gocart/store.go
@@ -0,0 +1,75 @@
+package gocart
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+type ConfigSpec struct {
+	name     string
+	path     string
+	platform string
+}
+type GoCartState struct {
+	configs  map[string]ConfigSpec
+	platform string
+}
+
+type KeyValueStore struct {
+	path string
+}
+
+func (*KeyValueStore) Serialize(state GoCartState) error {
+	stateData, err := json.Marshal(state)
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(MappingFilePath, stateData, 0640)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (*KeyValueStore) Deserialize() (GoCartState, error) {
+	var state GoCartState
+	kvFile, err := ioutil.ReadFile(MappingFilePath)
+	if err != nil {
+		return state, err
+	}
+	err = json.Unmarshal([]byte(kvFile), &state)
+	if err != nil {
+		return state, err
+	}
+	return state, nil
+}
+
+func (kvStore *KeyValueStore) Put(cfg ConfigSpec) error {
+	kvState, err := kvStore.Deserialize()
+	if err != nil {
+		return err
+	}
+	kvState.configs[cfg.name] = cfg
+	err = kvStore.Serialize(kvState)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (kvStore *KeyValueStore) Get(name string) (ConfigSpec, error) {
+	kvState, err := kvStore.Deserialize()
+	if err != nil {
+		return ConfigSpec{}, err
+	}
+	return kvState.configs[name], nil
+}
+
+func (kvStore *KeyValueStore) GetAll() (map[string]ConfigSpec, error) {
+	cfgs := make(map[string]ConfigSpec)
+	kvState, err := kvStore.Deserialize()
+	if err != nil {
+		return cfgs, err
+	}
+	return kvState.configs, nil
+}
--- main.go
+++ main.go
@@ -2,87 +2,17 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"flag"
-	"io/ioutil"
 	"os"
 
+	"github.com/bendavidaaron/gocart"
 	"github.com/google/subcommands"
 )
 
-type ConfigSpec struct {
-	name     string
-	path     string
-	platform string
-}
-
-type GoCartState struct {
-	configs  map[string]ConfigSpec
-	platform string
-}
-
-const mappingFilePath string = ".gocart.map"
-
-type KeyValueStore struct {
-	path string
-}
-
-func (*KeyValueStore) Serialize(state GoCartState) error {
-	stateData, err := json.Marshal(state)
-	if err != nil {
-		return err
-	}
-	err = ioutil.WriteFile(mappingFilePath, stateData, 0640)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (*KeyValueStore) Deserialize() (GoCartState, error) {
-	var state GoCartState
-	kvFile, err := ioutil.ReadFile(mappingFilePath)
-	if err != nil {
-		return state, err
-	}
-	err = json.Unmarshal([]byte(kvFile), &state)
-	if err != nil {
-		return state, err
-	}
-	return state, nil
-}
-
-func (kvStore *KeyValueStore) Put(cfg ConfigSpec) error {
-	kvState, err := kvStore.Deserialize()
-	if err != nil {
-		return err
-	}
-	kvState.configs[cfg.name] = cfg
-	err = kvStore.Serialize(kvState)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (kvStore *KeyValueStore) Get(name string) (ConfigSpec, error) {
-	kvState, err := kvStore.Deserialize()
-	if err != nil {
-		return ConfigSpec{}, err
-	}
-	return kvState.configs[name], nil
-}
-
-func (kvStore *KeyValueStore) GetAll() (map[string]ConfigSpec, error) {
-	cfgs := make(map[string]ConfigSpec)
-	kvState, err := kvStore.Deserialize()
-	if err != nil {
-		return cfgs, err
-	}
-	return kvState.configs, nil
-}
+const MappingFilePath string = ".gocart.json"
 
 type initCmd struct {
+	platform_name string
 }
 
 func (*initCmd) Name() string     { return "init" }
@@ -90,10 +20,14 @@ func (*initCmd) Synopsis() string { return "create a gocart mapping file in the 
 func (*initCmd) Usage() string    { return "init" }
 
 func (cmd *initCmd) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
-	os.Create(mappingFilePath)
+	os.Create(MappingFilePath)
+	initState := gocart.GoCartState{platform: f.platform}
+	kvStore := KeyValueStore{path: MappingFilePath}
+
 	return subcommands.ExitSuccess
 }
 func (cmd *initCmd) SetFlags(f *flag.FlagSet) {
+	f.StringVar(&cmd.platform_name, "p", "", "platform name (OSX, BSD, Linux)")
 }
 
 type addCmd struct {
