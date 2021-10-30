744813fb6b8e598a70010bb400c441520c247fbd
Ben Aaron <ben@theaarons.com>
2021-10-30 11:52:13 -0700

    init cobra
    add init command
    
    Revert "init cobra"
    
    This reverts commit eafe4977404c5538b9c57805937de3570defb7d6.
    
    fix license headers

--- main.go
+++ main.go
@@ -1,87 +1,23 @@
+/*
+Copyright © 2021 Ben Aaron <ben@betadeltaalpha.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 package main
 
-import (
-	"context"
-	"flag"
-	"os"
-
-	"internal/gocart"
-
-	"github.com/google/subcommands"
-)
-
-type initCmd struct {
-	platform_name string
-}
-
-func (*initCmd) Name() string     { return "init" }
-func (*initCmd) Synopsis() string { return "create a gocart mapping file in the current directory" }
-func (*initCmd) Usage() string    { return "init" }
-
-func (cmd *initCmd) SetFlags(f *flag.FlagSet) {
-	f.StringVar(&cmd.platform_name, "p", "", "platform name (OSX, BSD, Linux)")
-}
-
-func (cmd *initCmd) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
-	os.Create(gocart.MappingFilePath)
-	initState := gocart.GoCartState{Platform: cmd.platform_name}
-	kvStore := gocart.KeyValueStore{Path: gocart.MappingFilePath}
-	kvStore.Serialize(initState)
-	return subcommands.ExitSuccess
-}
-
-type addCmd struct {
-	config_name      string
-	config_file_path string
-}
-
-func (*addCmd) Name() string     { return "add" }
-func (*addCmd) Synopsis() string { return "add a config file to the mapping in the current directory" }
-func (*addCmd) Usage() string {
-	return `add <config_file_path> <config_name>
-    add a config file to the mapping in the current directory
-	`
-}
-
-func (cmd *addCmd) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
-	// open config file and insert a new key:value pair
-	return subcommands.ExitSuccess
-}
-func (cmd *addCmd) SetFlags(f *flag.FlagSet) {
-	f.StringVar(&cmd.config_file_path, "p", "", "path to config file to track")
-	f.StringVar(&cmd.config_name, "n", "", "name of a config")
-}
-
-type removeCmd struct {
-	config_name string
-}
-
-func (*removeCmd) Name() string { return "remove" }
-func (*removeCmd) Synopsis() string {
-	return "remove a config file from the local mapping and restore it to it's directory"
-}
-func (*removeCmd) Usage() string {
-	return `remove <config_name>
-	remove a config file from the local mapping and restore it to it's directory
-	`
-}
-
-func (cmd *removeCmd) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
-	// open config file and insert a new key:value pair
-	return subcommands.ExitSuccess
-}
-func (cmd *removeCmd) SetFlags(f *flag.FlagSet) {
-	f.StringVar(&cmd.config_name, "config_name", "", "name of the config file to untrack")
-}
+import "github.com/bendavidaaron/gocart/cmd"
 
 func main() {
-	subcommands.Register(subcommands.HelpCommand(), "")
-	subcommands.Register(subcommands.FlagsCommand(), "")
-	subcommands.Register(subcommands.CommandsCommand(), "")
-	subcommands.Register(&initCmd{}, "")
-	subcommands.Register(&addCmd{}, "")
-
-	flag.Parse()
-	ctx := context.Background()
-	os.Exit(int(subcommands.Execute(ctx)))
+	cmd.Execute()
 }
