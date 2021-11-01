eb265cfdbd8f7b32161684f503d2f7513ee478e8
Ben Aaron <ben@theaarons.com>
2021-11-01 10:03:16 -0700

    add tasks for config ops

--- internal/commands.go
+++ internal/commands.go
@@ -35,3 +35,46 @@ func SetPlatform(newPlatform string) error {
 	}
 	return nil
 }
+
+func GetConfigSpec(cfgName string) (ConfigSpec, error) {
+	// Get a config spec from the current dir's gocart mapping
+	var cfg ConfigSpec
+	gcState, err := ReadGoCartState()
+	if err != nil {
+		return cfg, err
+	}
+	cfg = gcState.configs[cfgName]
+	return cfg, nil
+}
+
+func PutConfigSpec(cfg ConfigSpec) error {
+	// Put a new config spec in the gocart mapping file
+	gcState, err := ReadGoCartState()
+	if err != nil {
+		return err
+	}
+	// TODO: Copy config file to workdir
+	// TODO: Backlink config file from workdir to cfg.Path
+	gcState.configs[cfg.name] = cfg
+	err = WriteGocartState(gcState)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func DeleteConfigSpec(cfgName string) error {
+	// Remove a config spec from the current dir's gocart mapping
+	gcState, err := ReadGoCartState()
+	if err != nil {
+		return err
+	}
+	// TODO: move config file to cfg.Path
+	// TODO: remove backlink from repo's config file
+	delete(gcState.configs, cfgName)
+	err = WriteGocartState(gcState)
+	if err != nil {
+		return err
+	}
+	return nil
+}
